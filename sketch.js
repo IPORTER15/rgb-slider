4473a96ec9c64afdb4a44b9d660f80fa7fb6e6e7
Isaac Porter <iporter@wlapcs.org>
2018-01-22 18:56:59 +0000

    added sliders amd background changing

--- sketch.js
+++ sketch.js
@@ -1,8 +1,23 @@
+var porterSlider;  genaeSlider, bakerSlider
+
 function setup(){
     createCanvas(600,400);
     background(0);
+    
+    porterSlider = createSlider(0, 255, 0);
+    porterSlider.position(20,20);
+    
+    genaeSlider = createSlider(0, 255, 0);
+    genaeSlider.position(20,50);
+    
+    bakerSlider = createSlider(0, 255, 0);
+    bakerSlider.position(20,80);
 }
 
 function draw(){
+    redRuby = porterSlider.value();
+    greenSavage = genaeSlider.value();
+    blueKiller = bakerSlider.value();
     
+    background(redRuby, greenSavage, blueKiller);
 }
